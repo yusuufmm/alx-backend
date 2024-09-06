95308b897ab7cf548ce4c20651dcb9125a448e91
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2024-09-05 18:31:56 -0700

    Tasks

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,8 +5,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page.
-    """
+
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)"
